f236f30b5748ad250717dcf1e2df7880ebcca0bc
Lucas Vieira <lucas@vieira.io>
2015-10-02 15:03:16 -0300

    braille array cursor

--- lib/didv_beta/braille_array_cursor.rb
+++ lib/didv_beta/braille_array_cursor.rb
@@ -1,4 +1,98 @@
 module DIDVBeta
   class BrailleArrayCursor
+
+    attr_reader :position
+    attr_accessor :end_of_text, :columns
+
+    def initialize(end_of_text = 0)
+      @end_of_text = end_of_text
+      @position = @end_of_text
+    end
+
+    def column
+      @position % @columns
+    end
+
+    def line
+      @position / @columns
+    end
+
+    def jump(new_position)
+      case new_position
+      when :start_of_line then go_to_start_of_line
+      when :end_of_line then go_to_end_of_line
+      when :start_of_text then go_to_start_of_text
+      when :end_of_text then go_to_end_of_text
+      when :next_line then go_to_next_line
+      when :last_line then go_to_last_line
+      else @position = new_position if new_position.between? 0, @end_of_text
+      end
+    end
+
+    def step(direction)
+      case direction
+      when :forward then step_forward
+      when :back then step_back
+      end
+    end
+
+    def start_of_line?
+      column == 0
+    end
+
+    def end_of_line?
+      column == @columns - 1 or @position == @end_of_text
+    end
+
+    def start_of_text?
+      @position == 0
+    end
+
+    def end_of_text?
+      @position == @end_of_text
+    end
+
+    private
+
+    def go_to_start_of_line
+      @position = line * @columns
+    end
+
+    def go_to_end_of_line
+      end_of_line = (line * @columns) + @columns - 1
+      end_of_text_or end_of_line
+    end
+
+    def go_to_start_of_text
+      @position = 0
+    end
+
+    def go_to_end_of_text
+      @position = @end_of_text
+    end
+
+    def go_to_next_line
+      next_line = ( line + 1 ) * @columns + column
+      end_of_text_or next_line unless line == @end_of_text / @columns
+    end
+
+    def go_to_last_line
+      last_line = ( line - 1 ) * @columns + column
+      jump(last_line)
+    end
+
+    def step_forward
+      end_of_text_or @position + 1
+    end
+
+    def step_back
+      @position -= 1 unless @position == 0
+    end
+
+    def end_of_text_or(position)
+      position = @end_of_text if position > @end_of_text
+      @position = position
+    end
+
   end
 end
--- spec/lib/didv_beta/braille_array_cursor_spec.rb
+++ spec/lib/didv_beta/braille_array_cursor_spec.rb
@@ -1,22 +1,130 @@
 describe BrailleArrayCursor, 'new' do
-  it "should be at start of text"
+  it "should be at start of text" do
+    expect(BrailleArrayCursor.new.position).to eq 0
+  end
+  it "should be at informed end of text position" do
+    @cursor = BrailleArrayCursor.new(10)
+    expect(@cursor.position).to eq 10
+  end
 end
 
 describe BrailleArrayCursor, 'jump' do
-  it "should jump to a given position"
-  it "should jump to start of line"
-  it "should jump to end of line"
-  it "should jump to start of text"
-  it "should jump to end of text"
-  it "should jump to the same column in the next line"
-  it "should jump to the same column in the last line"
-  it "shouldn't jump to a negative position"
-  it "shouldn't jump to a position beyond end of text"
+  before(:all) { @cursor = BrailleArrayCursor.new }
+  before(:each) do
+    @cursor.end_of_text = 99
+    @cursor.columns = 10
+    @cursor.jump(45)
+  end
+
+  it "should jump to a valid given position" do
+    new_position = rand(99)
+    @cursor.jump(new_position)
+    expect(@cursor.position).to eq new_position
+  end
+
+  it "should jump to start of line" do
+    new_position = rand(99)
+    @cursor.jump(new_position)
+    @cursor.jump(:start_of_line)
+    expect(@cursor.column).to eq 0
+    expect(@cursor.start_of_line?).to be true
+  end
+
+  it "should jump to end of line when line is full"  do
+    @cursor.jump(:end_of_line)
+    expect(@cursor.column).to eq 9
+    expect(@cursor.end_of_line?).to be true
+  end
+
+  it "should jump to end of line when line is not full" do
+    @cursor.end_of_text = 48
+    @cursor.jump(:end_of_line)
+    expect(@cursor.column).to eq 8
+    expect(@cursor.end_of_line?).to be true
+  end
+
+  it "should jump to start of text" do
+    @cursor.jump(:start_of_text)
+    expect(@cursor.position).to eq 0
+    expect(@cursor.start_of_text?).to be true
+  end
+
+  it "should jump to end of text" do
+    @cursor.jump(:end_of_text)
+    expect(@cursor.position).to eq 99
+    expect(@cursor.end_of_text?).to be true
+  end
+
+  it "should jump to the same column at next line when next line is full" do
+    @cursor.jump(:next_line)
+    expect(@cursor.position).to eq 55
+  end
+
+  it "should jump to the closer column at next line when next line isn't full" do
+    @cursor.end_of_text = 53
+    @cursor.jump(:next_line)
+    expect(@cursor.position).to eq 53
+  end
+
+  it "shouldn't jump to next line when at end_of_text line" do
+    @cursor.jump(91)
+    @cursor.jump(:next_line)
+    expect(@cursor.position).to eq 91
+  end
+
+  it "should jump to the same column in the last line" do
+    @cursor.jump(:last_line)
+    expect(@cursor.position).to eq 35
+  end
+
+  it "shouldn't jump to last line when at start_of_text line" do
+    @cursor.jump(5)
+    @cursor.jump(:last_line)
+    expect(@cursor.position).to eq 5
+  end
+
+  it "shouldn't jump to a negative position" do
+    @cursor.jump(-10)
+    expect(@cursor.position).to eq 45
+  end
+
+  it "shouldn't jump to a position beyond end of text" do
+    @cursor.jump(200)
+    expect(@cursor.position).to eq 45
+  end
+
 end
 
 describe BrailleArrayCursor, 'step' do
-  it "should step forward"
-  it "should step back"
-  it "shouldn't step back when at start of text"
-  it "shouldn't step forward when at end of text"
+  before(:all) { @cursor = BrailleArrayCursor.new }
+  before(:each) do
+    @cursor.end_of_text = 99
+    @cursor.columns = 10
+    @cursor.jump(45)
+  end
+
+  it "should step forward" do
+    @cursor.step(:forward)
+    expect(@cursor.position).to eq 46
+  end
+
+  it "should step back" do
+    @cursor.step(:back)
+    expect(@cursor.position).to eq 44
+  end
+
+  it "shouldn't step back when at start of text" do
+    @cursor.jump(:start_of_text)
+    @cursor.step(:back)
+    expect(@cursor.position).to eq 0
+    expect(@cursor.start_of_text?).to be true
+  end
+
+  it "shouldn't step forward when at end of text" do
+    @cursor.jump(:end_of_text)
+    @cursor.step(:forward)
+    expect(@cursor.position).to eq 99
+    expect(@cursor.end_of_text?).to be true
+  end
+
 end
